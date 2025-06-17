573f739ce44ac99439ac7a27b753cfcbe1da24d8
Szilveszter <nmszilveszter@gmail.com>
2025-06-17 09:28:40 +0200

    Created Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
-class todo(models.Model):
-    
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    created_at = models.DateField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+def __str__(self):
+        return self.title
