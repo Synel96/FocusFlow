0d6496f81da72a0efdd1ef81f067fe3bccfb144b
Szilveszter <nmszilveszter@gmail.com>
2025-06-17 11:11:33 +0200

    Adding Viewset and router

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,11 @@
 
-from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from todo.views import TodoViewSet
 from django.urls import path,include
 
-
+router = DefaultRouter()
+router.register(r'todo', TodoViewSet, basename='todo')
 urlpatterns = [
-    
+    path('api/', include(router.urls)),
+
 ]
